6f4d6218174c3731fe05f1be5de0e403cc1ac4a2
ODIN <odinnagust@protonmail.com>
2025-03-07 14:42:29 +0000

    event entity bætt við

--- app/src/main/java/is/hi/hbv601_team_12/data/entities/Event.kt
+++ app/src/main/java/is/hi/hbv601_team_12/data/entities/Event.kt
@@ -2,9 +2,27 @@ package `is`.hi.hbv601_team_12.data.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.LocalDateTime
 
-@Entity(tableName = "events")
-data class Event (
+@Entity(
+    tableName = "events",
+)
+data class Event(
     @PrimaryKey(autoGenerate = true)
-    val id: Int = 0
+    val id: Int = 0,
+    
+    var name: String,
+    var description: String?,
+    
+    var startDateTime: LocalDateTime,
+    var durationMinutes: Int, 
+    
+    var creatorId: Int,
+    
+    var location: String?,
+    
+    // ekki viss hvort þessi eru must
+    var isPublic: Boolean = true,
+    var maxParticipants: Int? = null
+    
 )

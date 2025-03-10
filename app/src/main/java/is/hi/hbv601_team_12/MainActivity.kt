c5f333bad278e1b76b93dbdd8f1ca2f7490a20d3
ODIN <odinnagust@protonmail.com>
2025-03-10 09:06:13 +0000

    engin email takki i horninu (commentadur ut)

--- app/src/main/java/is/hi/hbv601_team_12/MainActivity.kt
+++ app/src/main/java/is/hi/hbv601_team_12/MainActivity.kt
@@ -32,11 +32,11 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
+        // binding.appBarMain.fab.setOnClickListener { view ->
+        //     Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //         .setAction("Action", null)
+        //         .setAnchorView(R.id.fab).show()
+        // }
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
